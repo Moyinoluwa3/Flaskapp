0801b8e8b4b0e625e44d96d13ec4f9918318aaf3
Moyinoluwa3 <108367415+Moyinoluwa3@users.noreply.github.com>
2022-06-29 20:42:20 +0100

    Update __init__.py

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -19,7 +19,7 @@ app.config['MONGODB_SETTINGS'] = {
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = "465"
 app.config["MAIL_USERNAME"] = "moyogundare@gmail.com"
-app.config["MAIL_PASSWORD"] = "eberechi"
+app.config["MAIL_PASSWORD"] = "*"
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
